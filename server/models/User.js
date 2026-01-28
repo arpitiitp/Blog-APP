d9fe1a3186f247773185094f42810ecbd8261fdc
Apit Singh <diwakarkhiri1@gmail.com>
2026-01-29 01:17:45 +0530

    corrected signup

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   image: { type: String },
   googleId: { type: String, unique: true },
+  password: { type: String },
   role: { type: String, default: "user" },
 }, { timestamps: true });
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { googleLogin } from '../controllers/authController.js';
+import { googleLogin, register, login } from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/google', googleLogin);
+authRouter.post('/register', register);
+authRouter.post('/login', login);
 
 export default authRouter;
